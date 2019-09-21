d7ca033f175dd17038c6e81fc6311825f8d17339
Jamie Bliss <jamie@ivyleav.es>
2019-09-21 15:39:35 -0400

    Have cirrus do both docker and kaniko tests

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -t p2c .
-docker run --tmpfs /tmp -v testproj:/workspace -w /workspace p2c poetry2container
+docker run --tmpfs /tmp -v $(realpath testproj):/workspace -w /workspace p2c poetry2container
